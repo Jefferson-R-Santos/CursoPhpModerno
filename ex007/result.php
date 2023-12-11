ede20821e79202f2d836b013f48bc7616be60d20
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-12-11 13:25:19 -0300

    fadf

---
+++ ex007/result.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <main>
+
+<?php 
+
+$nale = random_int(1,100);
+
+echo "Gerando um numero aleatorio entre 1 e 100...";
+
+echo "O valor gerado foi "
+
+?>
+
+<p><a href="javascript:history.go(-1)">Voltar</a></p>
+
+</main>
+
+</body>
+</html>
