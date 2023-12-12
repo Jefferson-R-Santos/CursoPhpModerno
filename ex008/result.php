2948c8964e9460e2c3f3f4dcf391358cd38f5272
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-12-12 16:02:11 -0300

    Create result.php

---
+++ ex008/result.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <main>
+<section>
+    
+        <form>
+
+        
+        <?php 
+// cotação do google
+    $cotação = 4.96;
+// quantos reais vc tem
+    $real = 1000;
+
+// quantos dolares vc tem.
+    $dolar = $real/$cotação ;
+    
+    // mostrar o resultado
+    echo "Seus R/$". number_format($real, 2, ".", ",") . " equivalem a US/$".  number_format($dolar, 2);
+
+?>
+
+        <p><a href="javascript:history.go(-1)">Voltar</a></p>
+     
+     </form>
+    
+    </section>
+</main>
+
+</body>
+</html>
