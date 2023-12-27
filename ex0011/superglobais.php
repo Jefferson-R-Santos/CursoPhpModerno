07d2844b49dac00e170fdf92d2e04fa3e4fbc460
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-12-27 14:31:53 -0300

    Update superglobais.php

--- ex0011/superglobais.php
+++ ex0011/superglobais.php
@@ -14,18 +14,39 @@
     <?php
     
     setcookie("dia-da-semana", "Segunda", time() + 3600);
+    session_start();
+    $_SESSION["test"] = "Funcionou"
 
+    //Pega Dados pela URL
     echo "<h1>Super Global Get</h1>";
     var_dump($_GET);
-
+    
+    //Pega dados pelo cabeçalho
     echo "<h1><br>Super Global Post</h1>";
     var_dump($_POST);
-
+    
+    //Pega Dados em GET e POST
     echo "<h1><br>Super Global Request</h1>";
     var_dump($_REQUEST);
-
+    
+    //Salva dados do perfil e compornamentais
     echo "<h1><br>Super Global Cookie</h1>";
     var_dump($_COOKIE);
+
+    //Mantem sua sessão ativa
+    echo "<h1><br>Super Global Session</h1>";
+    var_dump($_SESSION);
+
+    //Mostra as variaveis de ambiente
+    echo "<h1><br>Super Global ENV</h1>";
+    var_dump($_ENV);
+    foreach (getenv() as $c > $v) {
+        echo "<br> $c -> $v";
+    }
+
+    //
+    echo "<h1><br>Super Global Server</h1>";
+    var_dump($_SERVER);
 
     ?>
 </pre>
