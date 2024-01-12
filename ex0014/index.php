c5567721470edd5705b6df1ee52280c08f5e7f24
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2024-01-12 16:43:08 -0300

    Update index.php

--- ex0014/index.php
+++ ex0014/index.php
@@ -8,13 +8,25 @@
 </head>
 <body>
     <h1>Calculando Sua Idade</h1>
+
+<?php 
+$atual = date("Y");
+$nasc = $_GET['nasc']?? 2000;
+$ano = $_GET['ano']?? $atual;
+?>
+
 <main>
     <form action="<?= $_SERVER['PHP_SELF'] ?> " method="get"></form>
     <label for="nasc"> Em que ano você nasceu?</label>
     <input type="number" name="nasc" id="nasc" min="1900">
-    <label for="ano">Quer saber sua idade em qual ano?</label>
-    <input type="number" name="ano" id="ano" max="<?=date("Y")?>">
+    <label for="ano">Quer saber sua idade em qual ano? (Atualmente estamos em <?= date("Y") ?>)</label>
+    <input type="number" name="ano" id="ano" max="<?=date("Y")?>" value="<?= $ano ?>">
+    <input type="submit" value="Qual será minha idade?">
 </main>
-
+<section>
+    <?php $idade = $ano - $nasc; ?>
+    <h2>Resultado:</h2>
+    <p>Quem nasceu em <?= $nasc ?> vai ter <strong><?= $idade ?> anos</strong> em <?= $ano ?>! </p>
+</section>
 </body>
 </html>
